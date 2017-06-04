364604eb18d573e965038771c5f2f544927d86b5
Sejo Vega-Cebrián <sejomagno@gmail.com>
2017-06-04 11:51:42 -0400

    url

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 
 var data;
 function preload(){
-	var url = 'http://itp.nyu.edu/camp2017/api/sessions_OfqSntpCAW.json';
+	var url = 'https://itp.nyu.edu/camp2017/api/sessions_OfqSntpCAW.json';
 	data = loadJSON(url);
 }
 function setup(){
